0ba36ac2161c9ebae42eef48f269e7854cb7ff7a
milesdwilliams15 <milesdwilliams15@gmail.com>
2021-10-14 14:33:53 -0500

    cleaning

--- 02_clean_and_merge/03_clean_archer_data.R
+++ 02_clean_and_merge/03_clean_archer_data.R
@@ -7,3 +7,32 @@
 
 rm(list = ls())
 library(tidyverse)
+
+
+# load data ---------------------------------------------------------------
+
+year <- as.list(2000:2014)
+dts <-
+  lapply(
+    year,
+    function(x) {
+      read_csv(
+        paste0(
+          str_remove(getwd(), "/git_version"),
+          "/Xinhua news articles_2000-2014/",
+          "Xinhua_", x, ".csv"
+        )
+      )
+    }
+  ) # useful for now to leave as a list
+
+
+
+# clean -------------------------------------------------------------------
+
+dts %>%
+  map(
+    ~ .x %>% pull(country) %>%
+      str_split("[|]") %>%
+      str_replace(".*,", "")
+  )
